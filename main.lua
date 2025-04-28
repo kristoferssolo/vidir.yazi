54ef22b31a1560d65253fb49ab811ae119910bac
Kristofers Solo <dev@kristofers.xyz>
2025-04-28 18:57:12 +0300

    fix: remove selectable item edit

--- main.lua
+++ main.lua
@@ -1,19 +1,9 @@
 --- @sync entry
+--- @since 25.2.7
 
 return {
 	entry = function()
-		local selected_items = cx.active.selected
-		if #selected_items >= 1 then
-			-- For selected files, pass them directly to vidir
-			local files = ""
-			for _, v in pairs(selected_items) do
-				files = files .. ya.quote(tostring(v)) .. " "
-			end
-
-			ya.manager_emit("shell", { "vidir " .. files, block = true, confirm = true })
-		else
-			-- If no selection, use current directory (.)
-			ya.manager_emit("shell", { "vidir .", block = true, confirm = true })
-		end
+		-- If no selection, use current directory (.)
+		ya.manager_emit("shell", { "vidir .", block = true, confirm = true })
 	end,
 }
