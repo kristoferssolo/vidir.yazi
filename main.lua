7e6217700caefb7fa88a2f3fbcea74ca632949c2
Kristofers Solo <dev@kristofers.xyz>
2025-04-28 17:54:48 +0300

    Initial commit

---
+++ main.lua
@@ -0,0 +1,14 @@
+return {
+	entry = function()
+		local selected_items = cx.active.selected
+		if #selected_items >= 1 then
+			local selected_urls = ""
+			for _, v in pairs(selected_items) do
+				selected_urls = selected_urls .. ya.quote(tostring(v))
+			end
+			ya.manager_emit("shell", { 'vidir "$@"', block = true, confirm = true })
+		else
+			ya.manager_emit("rename", { hovered = false, cursor = "before_ext" })
+		end
+	end,
+}
